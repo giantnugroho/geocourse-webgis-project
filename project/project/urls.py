3285865dd4efcca47878e94137f74f9efd0e10ee
giantnugroho <139608841+giantnugroho@users.noreply.github.com>
2023-11-13 18:03:52 +0700

    Step 8 - Update & Delete Objek Fasilitas

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bikini_bottom.views import home, home_map_api, custom_map_api, facility_form_add, facility_list
+from bikini_bottom.views import home, home_map_api, custom_map_api, facility_form_add, facility_list, facility_form_update, facility_form_delete
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,7 +25,9 @@ urlpatterns = [
     path('', home, name='home'),
     path('api/home-map/', home_map_api),
     path('api/custom-map/', custom_map_api),
-    path('facility/add/', facility_form_add),
+    path('facility/add/', facility_form_add, name='facility_form_add'),
+    path('facility/update/<int:pk>/', facility_form_update, name='facility_form_update'),
+    path('facility/delete/<int:pk>/', facility_form_delete, name='facility_form_delete'),
     path('facility/', facility_list, name='facility_list'),
 
     # Authentification
